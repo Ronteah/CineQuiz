994642ea5a2df63bc3710dfc8784606b2e5efdf6
julien ratouit <77002747+julien-ratouit@users.noreply.github.com>
2023-03-13 17:52:33 +0100

    TP1

--- app/src/main/java/com/example/cinequiz/MainActivity.java
+++ app/src/main/java/com/example/cinequiz/MainActivity.java
@@ -3,16 +3,32 @@ package com.example.cinequiz;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button stat;
     AlertDialog dialog;
+    EditText nomInput;
+    EditText prenomInput;
 
+    public String getNom() {
+        return nom;
+    }
+
+    String nom;
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    String prenom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +37,46 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
 
-        stat = findViewById(R.id.stat);
+        stat = findViewById(R.id.valider);
 
-        buildDialog();
+        this.nomInput = (EditText) findViewById(R.id.nom);
+
+        this.prenomInput = (EditText) findViewById(R.id.prenom);
+
+        this.prenom = this.prenomInput.getText().toString();
+        this.nom = this.nomInput.getText().toString();
+
+//        stat.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                buildDialog();
+//            }
+//        });
         stat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.show();
+                Intent intent = new Intent(MainActivity.this, ModeActivity.class);
+                intent.putExtra("nom", nom);
+                intent.putExtra("prenom", prenom);
+                startActivity(intent);
             }
         });
     }
 
     private void buildDialog() {
+        String prenom = this.prenomInput.getText().toString();
+        String nom = this.nomInput.getText().toString();
+
         AlertDialog.Builder builder = new AlertDialog.Builder((this));
-        //des choses
+        builder.setMessage("nom: " + nom + "\nprenom: " + prenom)
+                .setTitle("Info")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Action à effectuer lorsque l'utilisateur clique sur le bouton OK
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/cinequiz/ModeActivity.java
+++ app/src/main/java/com/example/cinequiz/ModeActivity.java
@@ -2,13 +2,29 @@ package com.example.cinequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ModeActivity extends AppCompatActivity {
+
+    String prenom;
+
+    String nom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
+
+        Intent intent = getIntent();
+
+        this.prenom = intent.getStringExtra(prenom);
+
+        this.nom = intent.getStringExtra(nom);
+
+        TextView bou = findViewById(R.id.bou);
+
+        bou.setText("bonjour " + prenom + " " + nom);
     }
 }
