38267cc5333fba880d8e1ccb677d79091ec8df85
julien-ratouit <77002747+julien-ratouit@users.noreply.github.com>
2023-03-13 13:09:56 +0100

    fix

--- app/src/main/java/com/example/cinequiz/MainActivity.java
+++ app/src/main/java/com/example/cinequiz/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.cinequiz;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button stat;
+    AlertDialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
+
+        stat = findViewById(R.id.stat);
+
+        buildDialog();
+        stat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
+    }
+
+    private void buildDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder((this));
+        //des choses
     }
 }
