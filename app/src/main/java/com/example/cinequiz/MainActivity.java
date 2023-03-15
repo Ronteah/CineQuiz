88c28ba0388047d78ce00573841fddc8eb8055ba
Louis Georget <louis.georget.etu@univ-lemans.fr>
2023-03-15 23:04:06 +0100

    Accueil, Stat, Niveaux

--- app/src/main/java/com/example/cinequiz/MainActivity.java
+++ app/src/main/java/com/example/cinequiz/MainActivity.java
@@ -1,82 +1,93 @@
 package com.example.cinequiz;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button stat;
-    AlertDialog dialog;
-    EditText nomInput;
-    EditText prenomInput;
-
-    public String getNom() {
-        return nom;
-    }
-
-    String nom;
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    String prenom;
+    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        getSupportActionBar().hide(); //hide the title bar
+//        getSupportActionBar().hide(); //hide the title bar
+        Objects.requireNonNull(getSupportActionBar()).hide(); //hide the title bar
         setContentView(R.layout.activity_main);
 
-        stat = findViewById(R.id.valider);
+        ImageButton easy = findViewById(R.id.btnEasy);
+        ImageButton medium = findViewById(R.id.btnMedium);
+        ImageButton hard = findViewById(R.id.btnHard);
 
-        this.nomInput = (EditText) findViewById(R.id.nom);
-
-        this.prenomInput = (EditText) findViewById(R.id.prenom);
-
-        this.prenom = this.prenomInput.getText().toString();
-        this.nom = this.nomInput.getText().toString();
-
-//        stat.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                buildDialog();
-//            }
-//        });
-        stat.setOnClickListener(new View.OnClickListener() {
+        easy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ModeActivity.class);
-                intent.putExtra("nom", nom);
-                intent.putExtra("prenom", prenom);
+                intent.putExtra("difficulty", "easy");
+                startActivity(intent);
+            }
+        });
+
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ModeActivity.class);
+                intent.putExtra("difficulty", "medium");
+                startActivity(intent);
+            }
+        });
+
+        hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ModeActivity.class);
+                intent.putExtra("difficulty", "hard");
                 startActivity(intent);
             }
         });
     }
 
-    private void buildDialog() {
-        String prenom = this.prenomInput.getText().toString();
-        String nom = this.nomInput.getText().toString();
-
-        AlertDialog.Builder builder = new AlertDialog.Builder((this));
-        builder.setMessage("nom: " + nom + "\nprenom: " + prenom)
-                .setTitle("Info")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Action à effectuer lorsque l'utilisateur clique sur le bouton OK
-                    }
-                });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 > x2){
+                    Intent intent = new Intent(MainActivity.this, StatActivity.class);
+                    startActivity(intent);
+                }
+                break;
+        }
+        return false;
     }
+
+//    private void buildDialog() {
+//        String prenom = this.prenomInput.getText().toString();
+//        String nom = this.nomInput.getText().toString();
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder((this));
+//        builder.setMessage("nom: " + nom + "\nprenom: " + prenom)
+//                .setTitle("Info")
+//                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // Action à effectuer lorsque l'utilisateur clique sur le bouton OK
+//                    }
+//                });
+//
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
+//    }
 }
--- app/src/main/java/com/example/cinequiz/ModeActivity.java
+++ app/src/main/java/com/example/cinequiz/ModeActivity.java
@@ -4,27 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Objects;
 
 public class ModeActivity extends AppCompatActivity {
-
-    String prenom;
-
-    String nom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        Objects.requireNonNull(getSupportActionBar()).hide(); //hide the title bar
         setContentView(R.layout.activity_mode);
 
         Intent intent = getIntent();
+        String difficulty = intent.getStringExtra("difficulty");
 
-        this.prenom = intent.getStringExtra(prenom);
+        ImageView textDifficulty = findViewById(R.id.textDifficulty);
 
-        this.nom = intent.getStringExtra(nom);
+        System.out.println(difficulty);
 
-        TextView bou = findViewById(R.id.bou);
+        switch (difficulty){
+            case "easy":
+                textDifficulty.setImageResource(R.drawable.facile);
+                break;
+            case "medium":
+                textDifficulty.setImageResource(R.drawable.moyen);
+                break;
+            case "hard":
+                textDifficulty.setImageResource(R.drawable.difficile);
+                break;
+        }
 
-        bou.setText("bonjour " + prenom + " " + nom);
+        ImageButton btnBack = findViewById(R.id.btnBack);
+
+        ImageButton btnNormal = findViewById(R.id.modeNormal);
+        ImageButton btnBlindtest = findViewById(R.id.modeBlindtest);
+        ImageButton btnImage = findViewById(R.id.modeImage);
+        ImageButton btnCelebrity = findViewById(R.id.modeCelebrity);
+        ImageButton btnReplique = findViewById(R.id.modeReplique);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ModeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
