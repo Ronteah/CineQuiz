77d42a6aee3bd6e02d2233e988032adf0036e4f9
Louis Georget <louis.georget.etu@univ-lemans.fr>
2023-03-22 15:17:18 +0100

    suppr 1er try motion

--- app/src/main/java/com/example/cinequiz/MainActivity.java
+++ app/src/main/java/com/example/cinequiz/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.cinequiz;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
@@ -13,8 +11,6 @@ import android.widget.ImageButton;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,24 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    public boolean onTouchEvent(MotionEvent touchEvent){
-        switch(touchEvent.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                x1 = touchEvent.getX();
-                y1 = touchEvent.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = touchEvent.getX();
-                y2 = touchEvent.getY();
-                if(x1 > x2){
-                    Intent intent = new Intent(MainActivity.this, StatActivity.class);
-                    startActivity(intent);
-                }
-                break;
-        }
-        return false;
     }
 
 //    private void buildDialog() {
