336d1d8c12fd272a953b7ae1a4c6f1e43c9d1a11
julien-ratouit <77002747+julien-ratouit@users.noreply.github.com>
2023-03-22 19:53:19 +0100

    game activity

--- app/src/main/java/com/example/cinequiz/GameActivity.java
+++ app/src/main/java/com/example/cinequiz/GameActivity.java
@@ -2,32 +2,13 @@ package com.example.cinequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.widget.RelativeLayout;
 
-import java.util.Objects;
-
-
-public class GameActivity extends AppCompatActivity{
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        Objects.requireNonNull(getSupportActionBar()).hide(); //hide the title bar
-        setContentView(R.layout.activity_stat);
-
-        RelativeLayout home = findViewById(R.id.btnHome);
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GameActivity.this, MainActivity.class);
-                intent.putExtra("difficulty", "easy");
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_game);
     }
 }
